a1c70ab887b768ecdf62765d12ff8c8a484d2a95
hack4Lucy <hack4Lucy@gmail.com>
2016-09-04 00:39:36 +0200

    Euler project ID-1 and commit

---
+++ Euler_project/EulerID-1/logic_EulerID-1.js
@@ -0,0 +1,32 @@
+
+/* ---------------------------------------------------------	*/
+/* This function is written to solve Euler problem 1 (ID-1)   */
+/* Developer: ask4Lucy (Email: ask4Lucy@gmail.com)            */
+/* Date: Saturday, 03SEP2016                                  */
+/* ---------------------------------------------------------	*/
+
+// Get access to input/output area
+var box_input  =document.getElementById('max_value');
+var box_output =document.getElementById('answer');
+
+// This function calculates the sum of multiples of 3 or 5 below the maximum natural number provided by the user
+function sumOfMultiples() {
+
+	// Get the input value
+	var max_value = box_input.value;
+
+	// Initialize the SUM variable
+	var sum = 0;
+
+	for (var i = 0; i < max_value; i++) {
+		if ((i%3 == 0) || (i%5 == 0)) {
+			sum += i;
+		}
+	}
+
+	// Output the answer
+	box_output.value =sum;
+
+	// clear input area
+	box_input.value ='';
+};
---
+++ Euler_project/Template/logic_EulerID-X.js
@@ -0,0 +1,13 @@
+
+/* ---------------------------------------------------------	*/
+/* This function is written to solve Euler problem X (ID-X)   */
+/* Developer: ask4Lucy (Email: ask4Lucy@gmail.com)            */
+/* Date: XXX                                                  */
+/* ---------------------------------------------------------	*/
+
+// Get access to input/output area
+// var box_input  =document.getElementById('max_value');
+// var box_output =document.getElementById('answer');
+
+// This function calculates XXX
+// @XXX: XXX
